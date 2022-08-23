11842b9138dbc4bf2df855876616ca75b3d5326e
AniCae PC <samuelychesterin@gmail.com>
2022-08-23 17:54:32 -0300

    creacion de ciudad y codigo js

---
+++ js/main.js
@@ -0,0 +1,41 @@
+//
+		// Variables
+		//
+
+		// Get the time right now
+		var now = new Date().getHours();
+
+		/**
+		 * Adjust the color theme based on time
+		 */
+		function adjustColorMode() {
+            var css=document.getElementById("css")
+
+
+			// If it's nighttime, go dark mode
+			if (now > 6 && now < 19) {
+                css.setAttribute("href", "/css/style.css");
+				return;
+			} else {
+                css.setAttribute("href", "/css/nightmode.css");
+            }
+		};
+
+		// /**
+		//  adjust the color palette
+		//  */
+		var updateUI = function () {
+
+			// Update color palette
+			adjustColorMode();
+
+		}
+
+		// Update the UI on page load
+		updateUI();
+
+		// Check again every 15 minutes
+		setInterval(function () {
+			now = new Date().getHours();
+			updateUI();
+		}, 1000 * 60 * 1);
